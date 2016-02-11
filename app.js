b200c392fbe77b13a6f77fe61822e92736ed5cfc
Craig Ellis <craigdavidellis@gmail.com>
2016-02-11 16:18:32 +0000

    Update Primary app routes for Dashboards

--- app.js
+++ app.js
@@ -128,6 +128,8 @@ app.post('/account/profile', passportConf.isAuthenticated, userController.postUp
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+app.get('/dashboards', userController.getLogin);
+app.post('/dashboards', userController.postLogin);
 
 /**
  * API examples routes.
